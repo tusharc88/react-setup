bf7fb978510a1eeb0d7f324e1563101a597f3c95
tusharc88 <tushar54983@gmail.com>
2018-04-07 21:20:34 -0700

    Changing App to Root

--- src/Root.js
+++ src/Root.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
 
-const App = () => (
+const Root = () => (
   <div>
     <h1>My React App</h1>
   </div>
 );
 
-export default hot(module)(App);
+export default hot(module)(Root);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
-import App from './App';
+import Root from './Root';
 
-render(<App />, document.getElementById('app'));
+render(<Root />, document.getElementById('root'));
